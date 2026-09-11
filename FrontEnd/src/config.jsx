d400a7a90ddb39d36c842a0ab153dbd77de6602e
Allem Hamed <114069826+C4ll-0f-Du5ty@users.noreply.github.com>
2026-09-11 15:45:30 +0300

    Update config.jsx

--- FrontEnd/src/config.jsx
+++ FrontEnd/src/config.jsx
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:8000"
-// export const BASE_URL = "https://atc-01017534727.onrender.com"
-export const BASE_URL = "https://eventory.allem.dev"
+export const BASE_URL = "https://atc-01017534727.onrender.com"
+// export const BASE_URL = "https://eventory.allem.dev"
